e88ba02f04135eac631a0a6196c30679865f662c
hqw <2060500128@qq.com>
2023-04-05 17:58:54 +0800

    second commit

--- src/main/java/com/hqw/git/GitText.java
+++ src/main/java/com/hqw/git/GitText.java
@@ -3,5 +3,12 @@ package com.hqw.git;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*" + i + "=" + i * j + "\t");
+            }
+            System.out.println();
+        }
     }
 }
